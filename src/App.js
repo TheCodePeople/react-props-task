17639ce5c3643b4db0a0aa3c988f3cabea680ecc
Mahdi209 <dr.arcadea@gmail.com>
2024-03-11 18:16:47 +0300

    props task is done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import ProfileCard from "./components/ProfileCard";
 import UserPosts from "./components/UserPosts";
 import FriendsList from "./components/FriendsList";
-
+import data from "./data";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <ProfileCard />
-      <UserPosts />
-      <FriendsList />
+      <ProfileCard
+        age={data.user.age}
+        bio={data.user.bio}
+        location={data.user.location}
+        name={data.user.name}
+      />
+      <UserPosts posts={data.posts} />
+      <FriendsList friends={data.friends} />
     </>
   );
 }
