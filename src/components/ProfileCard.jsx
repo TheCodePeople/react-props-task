ad93ff81507739ae7574019c447f8cbad7b42832
mohamedali0122 <mohamedalhelo909@gmail.com>
2024-03-12 21:03:43 +0300

    props task

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,10 +1,10 @@
-const ProfileCard = () => {
+const ProfileCard = ({name , age , bio , location}) => {
   return (
     <div className="profile-card">
-      <div className="profile-name">name</div>
-      <div className="profile-age">age</div>
-      <div className="profile-bio">bio</div>
-      <div className="profile-location">location</div>
+      <div className="profile-name">name : {name}</div>
+      <div className="profile-age">age : {age}</div>
+      <div className="profile-bio">bio : {bio}</div>
+      <div className="profile-location">location {location}</div>
     </div>
   );
 };
