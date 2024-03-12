ae12a3042db8fad1fbff31b61b542010e74de4a1
Mustafa88A <mustafa0780500@gmail.com>
2024-03-12 17:12:54 +0300

    task props

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import UserPosts from "./components/UserPosts";
 import FriendsList from "./components/FriendsList";
 
 import "./App.css";
-
+import "./data";
+import data from "./data";
 function App() {
   return (
     <>
-      <ProfileCard />
-      <UserPosts />
-      <FriendsList />
+      <ProfileCard
+        name={data.user.name}
+        age={data.age}
+        bio={data.bio}
+        location={data.location}
+      />
+      <UserPosts posts={data.posts} />
+      <FriendsList friends={data.friends} />
     </>
   );
 }
