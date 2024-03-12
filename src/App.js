ec65983e3d76258ae7ff2efc43a98290a0f43ea1
bit-sercererx <ameerahmed202397@gmail.com>
2024-03-12 20:31:10 +0300

    task completed

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import UserPosts from "./components/UserPosts";
 import FriendsList from "./components/FriendsList";
 
 import "./App.css";
+import data from "./data";
 
 function App() {
   return (
     <>
-      <ProfileCard />
-      <UserPosts />
-      <FriendsList />
+      <ProfileCard
+        age={data.user.age}
+        bio={data.user.bio}
+        location={data.user.name}
+      />
+      <UserPosts posts={data.posts} />
+      <FriendsList friends={data.friends} />
     </>
   );
 }
--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,10 +1,14 @@
-const ProfileCard = () => {
+const ProfileCard = ( {name,age,bio,location}) => {
   return (
     <div className="profile-card">
       <div className="profile-name">name</div>
       <div className="profile-age">age</div>
       <div className="profile-bio">bio</div>
       <div className="profile-location">location</div>
+      <div className="profile-name"> Name:{name}</div>
+      <div className="profile-age"> age:{age}</div>
+      <div className="profile-bio">{bio}</div>
+      <div className="profile-location">{location}</div>
     </div>
   );
 };
